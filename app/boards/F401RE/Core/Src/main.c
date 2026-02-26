22bd35521580ab4b058cd51201932cb43d5594f7
asmi-g <asmi.gujral@gmail.com>
2026-02-25 21:22:41 -0500

    stage-2-issue-8-gui-development-microcontroller-prototyping: Initial commit, working RTOS, non-functional UART

--- app/boards/F401RE/Core/Src/main.c
+++ app/boards/F401RE/Core/Src/main.c
@@ -87,6 +87,7 @@ void StartUartTxTask(void *argument);
 void StartMotorTask(void *argument);
 
 /* USER CODE BEGIN PFP */
+#define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
 
 /* USER CODE END PFP */
 
@@ -352,6 +353,20 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
+/**
+  * @brief  Retargets the C library printf function to the USART.
+  *   None
+  * @retval None
+  */
+PUTCHAR_PROTOTYPE
+{
+  /* Place your implementation of fputc here */
+  /* e.g. write a character to the USART1 and Loop until the end of transmission */
+  HAL_UART_Transmit(&huart1, (uint8_t *)&ch, 1, 0xFFFF);
+
+  return ch;
+}
+
 
 
 /* USER CODE END 4 */
