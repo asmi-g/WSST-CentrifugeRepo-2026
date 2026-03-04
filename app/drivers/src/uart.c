8635e6e58d3cd7b6baa8d0a1efafabe4db53f7dc
asmi-g <asmi.gujral@gmail.com>
2026-03-03 21:33:30 -0500

    stage-2-issue-8-gui-development-microcontroller-prototyping: UART RX and TX functional w/ GUI mvp

--- app/drivers/src/uart.c
+++ app/drivers/src/uart.c
@@ -3,20 +3,70 @@
 
 static UART_HandleTypeDef *uart_handle;
 static uint8_t rx_byte;
+uint8_t rx_buffer[100];           // actual memory allocated
+volatile uint8_t rx_indx = 0;
+volatile uint8_t transfer_cplt = 0;
 
 void uart_init(UART_HandleTypeDef *huart)
 {
     uart_handle = huart;
+    HAL_UART_Receive_IT(uart_handle, &rx_byte, 1);
+
 }
 
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
 {
-  /* Prevent unused argument(s) compilation warning */
-  UNUSED(huart);
-  /* NOTE: This function should not be modified, when the callback is needed,
-           the HAL_UART_RxCpltCallback could be implemented in the user file
-   */
+  if (huart == uart_handle)
+  {
+    if (rx_byte != '\r')
+    {
+      rx_buffer[rx_indx++] = rx_byte;
+    }
+    else
+    {
+      rx_buffer[rx_indx] = '\0';  // terminate string
+      rx_indx = 0;
+      transfer_cplt = 1;          // signal task
+    }
+    // Echo back the received byte
+    HAL_UART_Transmit_IT(uart_handle, &rx_byte, 1);
+
+    HAL_UART_Receive_IT(uart_handle, &rx_byte, 1);
+  }
+
+  /*if (huart == uart_handle)
+  {
+    uint8_t i;
+    if(uart_handle->Instance == USART2){
+      if(rx_indx == 0){
+        for(i=0; i<100; i++){
+          rx_buffer[i] = 0;
+          HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
+        }
+      }
+
+      if(rx_byte[0] != 13){
+        rx_buffer[rx_indx++] = rx_byte[0];
+      }
+      else{
+        rx_indx = 0;
+        transfer_cplt = 1;
+        HAL_UART_Transmit(uart_handle, "\n\r", 2, HAL_MAX_DELAY);
+        if(!strcmp(rx_buffer, "ON"))
+        {
+          HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
+        }
+      }
+
+      // Restart interrupt reception
+      HAL_UART_Receive_IT(uart_handle, &rx_byte, 1);
+
+      // Echo received byte
+      HAL_UART_Transmit(uart_handle, &rx_byte, 1, HAL_MAX_DELAY);
+    }
+  }*/
 }
+
 
 /**
   * @brief  Retargets the C library printf function to the USART.
