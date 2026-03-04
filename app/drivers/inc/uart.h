0b4a6ff3f4b1addedd114610324c84a4fe7119c5
asmi-g <asmi.gujral@gmail.com>
2026-03-03 21:33:30 -0500

    stage-2-issue-8-gui-development-microcontroller-prototyping: UART RX and TX functional w/ GUI mvp

--- app/drivers/inc/uart.h
+++ app/drivers/inc/uart.h
@@ -3,6 +3,10 @@
 
 #include "stm32f4xx_hal.h"
 
+extern uint8_t rx_buffer[100];
+extern volatile uint8_t rx_indx;
+extern volatile uint8_t transfer_cplt;
+
 void uart_init(UART_HandleTypeDef *huart);
 
 void uart_start_rx_it(void);
