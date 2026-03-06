a5028816cb79168b474c1af2b5aa0fef2fdaf596
asmi-g <asmi.gujral@gmail.com>
2026-03-06 03:13:52 -0500

    stage-2-issue-8-gui-development-microcontroller-prototyping: PWM integrated with UART

--- app/drivers/inc/pwm.h
+++ app/drivers/inc/pwm.h
@@ -15,6 +15,6 @@ HAL_StatusTypeDef pwm_init(pwm_t *p, TIM_HandleTypeDef *htim, uint32_t channel);
 HAL_StatusTypeDef pwm_start(pwm_t *p);
 
 // duty_u8 uses an 8-bit interface (0 to 255), giving 256 discrete PWM duty levels
-HAL_StatusTypeDef pwm_set(pwm_t *p, uint8_t duty_u8);
+HAL_StatusTypeDef pwm_set(pwm_t *p, uint8_t duty);
 
 #endif
