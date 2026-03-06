524e1134028a0c382f1a004b1d556905bb8568e9
asmi-g <asmi.gujral@gmail.com>
2026-03-06 05:24:07 -0500

    stage-2-issue-8-gui-development-microcontroller-prototyping: Functional PWM over UART w/ GUI

--- app/drivers/src/uart.c
+++ app/drivers/src/uart.c
@@ -3,9 +3,6 @@
 
 static UART_HandleTypeDef *uart_handle;
 static uint8_t rx_byte;
-uint8_t rx_buffer[100];           // actual memory allocated
-volatile uint8_t rx_indx = 0;
-volatile uint8_t transfer_cplt = 0;
 
 void uart_init(UART_HandleTypeDef *huart)
 {
@@ -14,29 +11,49 @@ void uart_init(UART_HandleTypeDef *huart)
 
 }
 
+
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
 {
   if (huart == uart_handle)
   {
-    if (rx_byte != '\r')
+    static uint8_t rx_indx = 0;
+    static uint8_t rx_buffer_local[16];  // short buffer for one command
+    uart_message_t msg;
+
+    // Filter end of line
+    if (rx_byte != '\r' && rx_byte != '\n')
     {
-      if (rx_indx < sizeof(rx_buffer) - 1)
-      {
-          rx_buffer[rx_indx++] = rx_byte;
-      }
+      if (rx_indx < sizeof(rx_buffer_local) - 1)
+        rx_buffer_local[rx_indx++] = rx_byte;
     }
     else
     {
-      rx_buffer[rx_indx] = '\0';  // terminate string
-      rx_indx = 0;
-      transfer_cplt = 1;          // signal task
-    }
-    // Echo back the received byte
-    HAL_UART_Transmit_IT(uart_handle, &rx_byte, 1);
+      if (rx_indx > 0)
+      {
+        rx_buffer_local[rx_indx] = '\0'; // Null terminate
+        // Copy safely
+        strncpy(msg.command, (char*)rx_buffer_local, sizeof(msg.command));
+        msg.command[sizeof(msg.command)-1] = '\0';
+        osMessageQueuePut(uartRxMessageQueueHandle, &msg, 0, 0);
 
+        // Echo back complete line
+        //HAL_UART_Transmit_IT(uart_handle, rx_buffer_local, rx_indx);
+
+        rx_indx = 0; // Reset buffer
+      }
+      // Else: just ignore stray '\r' or '\n'
+    }
+    // Restart reception
     HAL_UART_Receive_IT(uart_handle, &rx_byte, 1);
+  
   }
 
+}
+
+void uart_tx(const char *str){
+  osMutexAcquire(uartMutexHandle, osWaitForever);
+  HAL_UART_Transmit(uart_handle, (uint8_t*)str, strlen(str), HAL_MAX_DELAY);
+  osMutexRelease(uartMutexHandle);
 }
 
 /**
